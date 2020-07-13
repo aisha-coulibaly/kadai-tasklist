c6e8c7b9b81d8cc6263ee533e3bbb65a03b8f017
aisha.coulibaly <creepcxxp78@gmail.com>
2020-07-13 03:55:00 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   get 'login', to: 'sessions#new'
@@ -11,7 +7,7 @@ Rails.application.routes.draw do
   
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:new, :create]
   resources :tasks
   
 end
